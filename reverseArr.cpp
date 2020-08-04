1b4f7ad5c1d16080433cb4e11cf7cd2cc9463052
Dpk Bhat <ddbhat99@gmail.com>
2020-08-04 14:14:35 +0530

    Clear

--- reverseArr.cpp
+++ reverseArr.cpp
@@ -4,14 +4,14 @@ using namespace std;
 
 void reverseArr(int arr[], int front, int end)
 {
-    if (front < end)
+    if (!(front < end))
     {
-        int temp = arr[front];
-        arr[front] = arr[end];
-        arr[end] = temp;
-        reverseArr(arr, ++front, ++end);
+        return;
     }
-    return;
+    int temp = arr[front];
+    arr[front] = arr[end];
+    arr[end] = temp;
+    reverseArr(arr, ++front, --end);
 }
 int main()
 {
@@ -20,13 +20,9 @@ int main()
     {
         arr[i] = i;
     }
+    reverseArr(arr, 0, 99);
     for (int i = 0; i < 100; i++)
     {
-        cout << arr[i] << " ";
-    }
-    reverseArr(arr, 0, 100);
-    for (int i = 0; i < 100; i++)
-    {
-        cout << arr[i] << endl;
+        cout << arr[i] << "\t";
     }
 }

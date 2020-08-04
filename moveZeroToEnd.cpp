a153cdc51b16a2b3dc76a5bbb8bd4f1adda9cd29
Dpk Bhat <ddbhat99@gmail.com>
2020-08-04 14:25:18 +0530

    gng on. Cards now

---
+++ moveZeroToEnd.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+moveZeorToEnd(int arr[], int n)
+{
+}
+
+int main()
+{
+    int arr[100];
+    for (int i = 0; i < 100; i++)
+    {
+        arr[i] = rand() % 3;
+    }
+    moveZeorToEnd(arr, sizeof(arr) / sizeof(arr[0]))
+}

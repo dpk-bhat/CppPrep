2c1bbb1953966411ebed4c39bd5d19906b3effce
Dpk Bhat <ddbhat99@gmail.com>
2020-08-05 16:01:38 +0530

    fisher Yates and BubbleSort dn

--- bubbleSort.cpp
+++ bubbleSort.cpp
@@ -2,6 +2,55 @@
 
 using namespace std;
 
+void swapInt(int arr[], int i, int randIndex)
+{
+    int temp = arr[i];
+    arr[i] = arr[randIndex];
+    arr[randIndex] = temp;
+}
+
+void swapInt(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void shuffel(int arr[], int n)
+{
+    for (int i = n - 1; i > 1; i--)
+    {
+        int randIndex = rand() % i;
+        swapInt(arr, i, randIndex);
+    }
+}
+
 int main()
 {
+    int arr[100];
+    for (int i = 0; i < 100; i++)
+    {
+        arr[i] = i;
+    }
+    shuffel(arr, 100);
+    cout << "Before sorting :" << endl;
+    for (int i = 0; i < 100; i++)
+    {
+        cout << arr[i] << endl;
+    }
+    for (int i = 0; i < 99; i++)
+    {
+        for (int j = 0; j < 100 - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                swapInt(&arr[j], &arr[j + 1]);
+            }
+        }
+    }
+    cout << "Afte sorting :" << endl;
+    for (int i = 0; i < 100; i++)
+    {
+        cout << arr[i] << endl;
+    }
 }
---
+++ fisherYates.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+void swapInt(int arr[], int i, int randIndex)
+{
+    int temp = arr[i];
+    arr[i] = arr[randIndex];
+    arr[randIndex] = temp;
+}
+
+void fisherYates(int arr[], int n)
+{
+    for (int i = n - 1; i > 1; i--)
+    {
+        int randIndex = rand() % i;
+        swapInt(arr, i, randIndex);
+    }
+}
+
+int main()
+{
+    int arr[100] = {0};
+    for (int i = 0; i < 100; i++)
+    {
+        arr[i] = i;
+    }
+    fisherYates(arr, 100);
+    for (int i = 0; i < 100; i++)
+    {
+        cout << arr[i] << "\t";
+    }
+}

aa944f65a21f8a84e018cb9f4798dc3b2ed7a876
Dpk Bhat <ddbhat99@gmail.com>
2020-08-13 02:19:32 +0530

    dfsByMe.cpp
    wirh a lot of reference form GfG

--- dfsByMe.cpp
+++ dfsByMe.cpp
@@ -10,7 +10,7 @@ class Graph
     vector<int> *adj;
     void dfsInternal(int v, bool visited[])
     {
-        cout << v;
+        cout << v << " ";
         visited[v] = true;
 
         for (auto &it : adj[v])
@@ -29,9 +29,38 @@ public:
         adj = new vector<int>[V];
     }
 
-    void addEdg(int v, int u)
+    void addEdge(int v, int u)
     {
         adj[v].push_back(u);
         adj[u].push_back(v);
     }
+
+    void DFS(int v)
+    {
+        bool *visited = new bool[v];
+        for (int i = 0; i < V; i++)
+        {
+            visited[i] = false;
+        }
+
+        dfsInternal(v, visited);
+    }
 };
+
+int main()
+{
+    // Create a graph given in the above diagram
+    Graph g(4);
+    g.addEdge(0, 1);
+    g.addEdge(0, 2);
+    g.addEdge(1, 2);
+    g.addEdge(2, 0);
+    g.addEdge(2, 3);
+    g.addEdge(3, 3);
+
+    cout << "Following is Depth First Traversal"
+            " (starting from vertex 2) \n";
+    g.DFS(2);
+
+    return 0;
+}
